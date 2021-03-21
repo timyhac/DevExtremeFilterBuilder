e70a2c2eae2cbd7cf3b5827fbb5e710a5dcbac46
timyhac <timyhac@gmail.com>
2021-03-21 23:11:13 +1100

    Entity Framework test

--- src/FilterBuilder.Tests/BasicTests.cs
+++ src/FilterBuilder.Tests/BasicTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,29 +6,43 @@ using Xunit;
 
 namespace FilterBuilder.Tests
 {
-    public class BasicTests
+    public class BasicTests : IDisposable
     {
 
-        readonly Product item0 =  new () { CurrentInventory = 9,     Cost = 4.99f,   Name = "Sauce",                 Category = ProductCategory.Food };
-        readonly Product item1 =  new () { CurrentInventory = 12,    Cost = 1.50f,   Name = "Beans",                 Category = ProductCategory.Food };
-        readonly Product item2 =  new () { CurrentInventory = 8,     Cost = 3.20f,   Name = "Chocolate",             Category = ProductCategory.Food };
-        readonly Product item3 =  new () { CurrentInventory = 8,     Cost = 175,     Name = "SuperHD Video Player",  Category = ProductCategory.Electronics };
-        readonly Product item4 =  new () { CurrentInventory = 3,     Cost = 110,     Name = "HD Video Player",       Category = ProductCategory.Electronics };
-        readonly Product item5 =  new () { CurrentInventory = 0,     Cost = 775,     Name = "SuperLED 50",           Category = ProductCategory.Electronics };
-        readonly Product item6 =  new () { CurrentInventory = 0,     Cost = 675,     Name = "SuperLED 42",           Category = ProductCategory.Electronics };
-        readonly Product item7 =  new () { CurrentInventory = 1,     Cost = 745f,    Name = "SuperLCD 55",           Category = ProductCategory.Electronics };
-        readonly Product item8 =  new () { CurrentInventory = 3,     Cost = 710f,    Name = "SuperLCD 42",           Category = ProductCategory.Electronics };
-        readonly Product item9 =  new () { CurrentInventory = 2,     Cost = 2125f,   Name = "SuperLCD 70",           Category = ProductCategory.Electronics };
-        readonly Product item10 = new () { CurrentInventory = 1,     Cost = 70f,     Name = "DesktopLED 19",         Category = ProductCategory.Electronics };
-        readonly Product item11 = new () { CurrentInventory = 3,     Cost = 120f,    Name = "Table",                 Category = ProductCategory.Furniture };
-        readonly Product item12 = new () { CurrentInventory = 8,     Cost = 70f,     Name = "Chair",                 Category = ProductCategory.Furniture };
-        readonly Product item13 = new () { CurrentInventory = 2,     Cost = 199.99f, Name = "Deluxe Office Chair",   Category = ProductCategory.Furniture };
-
+        readonly ProductsContext dbContext;
         readonly List<Product> items;
+
+        readonly Product item0 = new() { CurrentInventory = 9, Cost = 4.99f, Name = "Sauce", Category = ProductCategory.Food };
+        readonly Product item1 = new() { CurrentInventory = 12, Cost = 1.50f, Name = "Beans", Category = ProductCategory.Food };
+        readonly Product item2 = new() { CurrentInventory = 8, Cost = 3.20f, Name = "Chocolate", Category = ProductCategory.Food };
+        readonly Product item3 = new() { CurrentInventory = 8, Cost = 175, Name = "SuperHD Video Player", Category = ProductCategory.Electronics };
+        readonly Product item4 = new() { CurrentInventory = 3, Cost = 110, Name = "HD Video Player", Category = ProductCategory.Electronics };
+        readonly Product item5 = new() { CurrentInventory = 0, Cost = 775, Name = "SuperLED 50", Category = ProductCategory.Electronics };
+        readonly Product item6 = new() { CurrentInventory = 0, Cost = 675, Name = "SuperLED 42", Category = ProductCategory.Electronics };
+        readonly Product item7 = new() { CurrentInventory = 1, Cost = 745f, Name = "SuperLCD 55", Category = ProductCategory.Electronics };
+        readonly Product item8 = new() { CurrentInventory = 3, Cost = 710f, Name = "SuperLCD 42", Category = ProductCategory.Electronics };
+        readonly Product item9 = new() { CurrentInventory = 2, Cost = 2125f, Name = "SuperLCD 70", Category = ProductCategory.Electronics };
+        readonly Product item10 = new() { CurrentInventory = 1, Cost = 70f, Name = "DesktopLED 19", Category = ProductCategory.Electronics };
+        readonly Product item11 = new() { CurrentInventory = 3, Cost = 120f, Name = "Table", Category = ProductCategory.Furniture };
+        readonly Product item12 = new() { CurrentInventory = 8, Cost = 70f, Name = "Chair", Category = ProductCategory.Furniture };
+        readonly Product item13 = new() { CurrentInventory = 2, Cost = 199.99f, Name = "Deluxe Office Chair", Category = ProductCategory.Furniture };
+
 
         public BasicTests()
         {
             items = new() { item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13 };
+
+            dbContext = new ProductsContext();
+            dbContext.Database.EnsureCreated();
+
+            dbContext.Products.AddRange(items);
+            dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            dbContext.Products.RemoveRange(dbContext.Products);
+            dbContext.SaveChanges();
         }
 
 
@@ -126,6 +141,33 @@ namespace FilterBuilder.Tests
             var predicate = builder.Get<Product>(jsonFilter).Compile();
             var filteredLst = list.Where(predicate);
 
+        }
+
+        [Fact]
+        public void Test4()
+        {
+
+            var jsonFilter = @"[""Name"", ""endswith"", ""e""]";
+
+            var expectedFilteredList = new List<Product>()
+            {
+                item0, item2, item11
+            };
+
+            FilterBuilder builder = new();
+            var predicate = builder.Get<Product>(jsonFilter);
+
+
+            var actualList = dbContext.Products.Where(predicate).ToList();
+
+            Assert.True(ContainSameElements(expectedFilteredList, actualList));
+        }
+
+
+        bool ContainSameElements<T>(IEnumerable<T> A, IEnumerable<T> B)
+        {
+            bool areSame = !A.Except(B).Any() && A.Count() == B.Count();
+            return areSame;
         }
     }
 }
---
+++ src/FilterBuilder.Tests/ProductsContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterBuilder.Tests
+{
+    class ProductsContext : DbContext
+    {
+                
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("test");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasKey(c => c.Name);
+        }
+    }
+
+}
